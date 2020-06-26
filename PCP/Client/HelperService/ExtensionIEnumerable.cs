72565ea33a8f75d3aad59877ea0c5acd2fde5346
Marcelo Albarracin <elchelamiente_25@hotmail.com>
2020-06-26 17:23:00 -0300

    Se agrega metodo de extension  para realizar foreach async Agregar exige lote, despacho y serie. Se agregar inconos de FontAwesome

---
+++ PCP/Client/HelperService/ExtensionIEnumerable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCP.Client.HelperService
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Metodo de extension para metodos que implementan IENUMERABLE 
+        /// podes 
+        /// </summary>
+        public static IEnumerable<T> Map<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+
+        public static async Task ForEachAsync<T>(this IEnumerable<T> enumerable, Func<T, Task> action)
+        {
+            foreach (var item in enumerable)
+            {
+                await action(item);
+            }
+        }
+
+
+    }
+}
